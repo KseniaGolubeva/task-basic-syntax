ca17c0d51d04cf231c6ba1d72fbeef7801ee1208
George <shirshov.gera@bk.ru>
2022-10-19 15:01:03 +0300

    задание те теме синтаксис JS

--- src/BasicSyntax.js
+++ src/BasicSyntax.js
@@ -1,8 +1,36 @@
 export function romanToInteger(str) {
+    let translator = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000,
+    };
+
     let result = 0;
-    /*
-    Напишите код, преобразующий число из римской записи в арабскую.
-    На вход вы получите строку, ответом должно быть число.
-     */
+
+    for (let i = 0; i < str.length; i++) {
+        let digit = translator[str[i]];
+        let nextDigit = translator[str[i + 1]];
+
+        if (isNaN(digit)) {
+            break;
+        }
+
+        if (isNaN(nextDigit)) {
+            result += digit;
+            break;
+        }
+
+        if (digit >= nextDigit) {
+            result += digit;
+        } else {
+            result += nextDigit - digit;
+            i++;
+        }
+    }
+
     return result;
 }
